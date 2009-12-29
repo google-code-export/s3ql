104d33a14e196e00aebf494ecc48099343efe8b4
nikratio <nikratio@localhost>
2009-12-29 16:38:13 +0000

    Removed blanks.

--- src/llfuse/__init__.py
+++ src/llfuse/__init__.py
@@ -4,12 +4,11 @@ $Id$
 Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
 
 This program can be distributed under the terms of the GNU LGPL.
-'''
+''' 
 
 from __future__ import unicode_literals, division, print_function
 
 # Wildcard imports desired
 #pylint: disable-msg=W0401
-from llfuse.operations import *
-from llfuse.interface import * 
-
+from operations import *
+from interface import *
