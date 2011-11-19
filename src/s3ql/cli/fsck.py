8241e55ba92ec7b0a7695faf0b1b5aabd51811e0
Nikolaus Rath <Nikolaus@rath.org>
2011-11-19 16:02:08 -0500

    Implemented new, custom metadata dump format.

---
+++ src/s3ql/cli/fsck.py
@@ -0,0 +1,240 @@
+'''
+fsck.py - this file is part of S3QL (http://s3ql.googlecode.com)
+
+Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
+
+This program can be distributed under the terms of the GNU GPLv3.
+'''
+
+from __future__ import division, print_function, absolute_import
+from s3ql import CURRENT_FS_REV
+from s3ql.backends.common import get_bucket
+from s3ql.common import (get_bucket_cachedir, cycle_metadata, setup_logging, 
+    QuietError, get_seq_no, restore_metadata, dump_metadata, CTRL_INODE)
+from s3ql.database import Connection
+from s3ql.fsck import Fsck
+from s3ql.parse_args import ArgumentParser
+import apsw
+import cPickle as pickle
+import logging
+import os
+import stat
+import sys
+import textwrap
+import time
+
+log = logging.getLogger("fsck")
+
+def parse_args(args):
+
+    parser = ArgumentParser(
+        description="Checks and repairs an S3QL filesystem.")
+
+    parser.add_log('~/.s3ql/fsck.log')
+    parser.add_cachedir()
+    parser.add_authfile()
+    parser.add_debug_modules()
+    parser.add_quiet()
+    parser.add_version()
+    parser.add_storage_url()
+
+    parser.add_argument("--batch", action="store_true", default=False,
+                      help="If user input is required, exit without prompting.")
+    parser.add_argument("--force", action="store_true", default=False,
+                      help="Force checking even if file system is marked clean.")
+    parser.add_argument("--renumber-inodes", action="store_true", default=False,
+                      help="Renumber inodes to be stricly sequential starting from %d"
+                           % (CTRL_INODE+1))
+    options = parser.parse_args(args)
+
+    return options
+
+def main(args=None):
+
+    if args is None:
+        args = sys.argv[1:]
+
+    options = parse_args(args)
+    setup_logging(options)
+        
+    # Check if fs is mounted on this computer
+    # This is not foolproof but should prevent common mistakes
+    match = options.storage_url + ' /'
+    with open('/proc/mounts', 'r') as fh:
+        for line in fh:
+            if line.startswith(match):
+                raise QuietError('Can not check mounted file system.')
+    
+    bucket = get_bucket(options)
+    
+    cachepath = get_bucket_cachedir(options.storage_url, options.cachedir)
+    seq_no = get_seq_no(bucket)
+    param_remote = bucket.lookup('s3ql_metadata')
+    db = None
+    
+    if os.path.exists(cachepath + '.params'):
+        assert os.path.exists(cachepath + '.db')
+        param = pickle.load(open(cachepath + '.params', 'rb'))
+        if param['seq_no'] < seq_no:
+            log.info('Ignoring locally cached metadata (outdated).')
+            param = bucket.lookup('s3ql_metadata')
+        else:
+            log.info('Using cached metadata.')
+            db = Connection(cachepath + '.db')
+            assert not os.path.exists(cachepath + '-cache') or param['needs_fsck']
+    
+        if param_remote['seq_no'] != param['seq_no']:
+            log.warn('Remote metadata is outdated.')
+            param['needs_fsck'] = True
+            
+    else:
+        param = param_remote
+        assert not os.path.exists(cachepath + '-cache')
+        # .db might exist if mount.s3ql is killed at exactly the right instant
+        # and should just be ignored.
+       
+    # Check revision
+    if param['revision'] < CURRENT_FS_REV:
+        raise QuietError('File system revision too old, please run `s3qladm upgrade` first.')
+    elif param['revision'] > CURRENT_FS_REV:
+        raise QuietError('File system revision too new, please update your '
+                         'S3QL installation.')
+    
+    if param['seq_no'] < seq_no:
+        if bucket.is_get_consistent():
+            print(textwrap.fill(textwrap.dedent('''\
+                  Up to date metadata is not available. Probably the file system has not
+                  been properly unmounted and you should try to run fsck on the computer 
+                  where the file system has been mounted most recently.
+                  ''')))
+        else:
+            print(textwrap.fill(textwrap.dedent('''\
+                  Up to date metadata is not available. Either the file system has not
+                  been unmounted cleanly or the data has not yet propagated through the backend.
+                  In the later case, waiting for a while should fix the problem, in
+                  the former case you should try to run fsck on the computer where
+                  the file system has been mounted most recently
+                  ''')))
+    
+        print('Enter "continue" to use the outdated data anyway:',
+              '> ', sep='\n', end='')
+        if options.batch:
+            raise QuietError('(in batch mode, exiting)')
+        if sys.stdin.readline().strip() != 'continue':
+            raise QuietError()
+        
+        param['seq_no'] = seq_no
+        param['needs_fsck'] = True
+    
+    
+    if (not param['needs_fsck'] 
+        and ((time.time() - time.timezone) - param['last_fsck'])
+             < 60 * 60 * 24 * 31): # last check more than 1 month ago
+        if options.force or options.renumber_inodes:
+            log.info('File system seems clean, checking anyway.')
+        else:
+            log.info('File system is marked as clean. Use --force to force checking.')
+            return
+    
+    # If using local metadata, check consistency
+    if db:
+        log.info('Checking DB integrity...')
+        try:
+            # get_list may raise CorruptError itself
+            res = db.get_list('PRAGMA integrity_check(20)')
+            if res[0][0] != u'ok':
+                log.error('\n'.join(x[0] for x in res ))
+                raise apsw.CorruptError()
+        except apsw.CorruptError:
+            raise QuietError('Local metadata is corrupted. Remove or repair the following '
+                             'files manually and re-run fsck:\n'
+                             + cachepath + '.db (corrupted)\n'
+                             + cachepath + '.param (intact)')
+    else:
+        def do_read(fh):
+            os.close(os.open(cachepath + '.db.tmp', os.O_RDWR | os.O_CREAT | os.O_TRUNC,
+                             stat.S_IRUSR | stat.S_IWUSR)) 
+            db = Connection(cachepath + '.db.tmp', fast_mode=True)
+            restore_metadata(fh, db)
+            db.close()
+        db = bucket.perform_read(do_read, "s3ql_metadata") 
+        os.rename(cachepath + '.db.tmp', cachepath + '.db')
+        db = Connection(cachepath + '.db')
+    
+    # Increase metadata sequence no 
+    param['seq_no'] += 1
+    param['needs_fsck'] = True
+    bucket['s3ql_seq_no_%d' % param['seq_no']] = 'Empty'
+    pickle.dump(param, open(cachepath + '.params', 'wb'), 2)
+    
+    fsck = Fsck(cachepath + '-cache', bucket, param, db)
+    fsck.check()
+    
+    if fsck.uncorrectable_errors:
+        raise QuietError("Uncorrectable errors found, aborting.")
+        
+    if os.path.exists(cachepath + '-cache'):
+        os.rmdir(cachepath + '-cache')
+        
+    if options.renumber_inodes:
+        renumber_inodes(db)
+            
+    cycle_metadata(bucket)
+    param['needs_fsck'] = False
+    param['last_fsck'] = time.time() - time.timezone
+    param['last-modified'] = time.time() - time.timezone
+    bucket.perform_write(lambda fh: dump_metadata(fh, db) , "s3ql_metadata",
+                         metadata=param, is_compressed=True) 
+    pickle.dump(param, open(cachepath + '.params', 'wb'), 2)
+        
+    db.execute('ANALYZE')
+    db.execute('VACUUM')
+    db.close() 
+
+    if options.renumber_inodes:
+        print('',
+              'Inodes were renumbered. If this file system has been exported over NFS,',
+              'all NFS clients need to be restarted before mounting the S3QL file system ',
+              'again or data corruption may occur.', sep='\n')
+        
+
+def renumber_inodes(db):
+    '''Renumber inodes'''
+    
+    log.info('Renumbering inodes...')
+    total = db.get_val('SELECT COUNT(id) FROM inodes')
+    db.execute('CREATE TEMPORARY TABLE inode_ids AS '
+               'SELECT id FROM inodes WHERE id > ? ORDER BY id DESC', 
+               (max(total, CTRL_INODE),))
+    db.execute('CREATE INDEX IF NOT EXISTS ix_contents_inode ON contents(inode)')
+    try:
+        i = 0
+        cur = CTRL_INODE+1
+        for (id_,) in db.query("SELECT id FROM inode_ids"):
+            while True:
+                try:
+                    db.execute('UPDATE inodes SET id=? WHERE id=?', (cur, id_))
+                except apsw.ConstraintError:
+                    cur += 1
+                else:
+                    break            
+            
+            db.execute('UPDATE contents SET inode=? WHERE inode=?', (cur, id_))
+            db.execute('UPDATE contents SET parent_inode=? WHERE parent_inode=?', (cur, id_))
+            db.execute('UPDATE inode_blocks SET inode=? WHERE inode=?', (cur, id_))
+            db.execute('UPDATE symlink_targets SET inode=? WHERE inode=?', (cur, id_))
+            db.execute('UPDATE ext_attributes SET inode=? WHERE inode=?', (cur, id_))
+                                    
+            cur += 1
+            i += 1
+            if i % 5000 == 0:
+                log.info('..processed %d inodes so far..', i)
+        
+    finally:
+        db.execute('DROP TABLE inode_ids')
+        db.execute('DROP INDEX ix_contents_inode')
+
+        
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
---
+++ src/s3ql/common.py
@@ -0,0 +1,474 @@
+'''
+common.py - this file is part of S3QL (http://s3ql.googlecode.com)
+
+Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
+
+This program can be distributed under the terms of the GNU GPLv3.
+'''
+
+from __future__ import division, print_function, absolute_import
+from llfuse import ROOT_INODE
+import cPickle as pickle
+import hashlib
+import logging
+import os
+import stat
+import sys
+import tempfile
+import time
+import lzma
+
+# Buffer size when writing objects
+BUFSIZE = 256 * 1024
+
+log = logging.getLogger('common')
+        
+def setup_logging(options):        
+    root_logger = logging.getLogger()
+    if root_logger.handlers:
+        log.debug("Logging already initialized.")
+        return
+        
+    stdout_handler = add_stdout_logging(options.quiet)
+    if hasattr(options, 'log') and options.log:
+        root_logger.addHandler(options.log)
+        debug_handler = options.log  
+    else:
+        debug_handler = stdout_handler
+    setup_excepthook()
+    
+    if options.debug:
+        root_logger.setLevel(logging.DEBUG)
+        debug_handler.setLevel(logging.NOTSET)
+        if 'all' not in options.debug:
+            # Adding the filter to the root logger has no effect.
+            debug_handler.addFilter(LoggerFilter(options.debug, logging.INFO))
+        logging.disable(logging.NOTSET)
+    else:
+        root_logger.setLevel(logging.INFO)
+        logging.disable(logging.DEBUG)
+        
+    return stdout_handler 
+ 
+                        
+class LoggerFilter(object):
+    """
+    For use with the logging module as a message filter.
+    
+    This filter accepts all messages which have at least the specified
+    priority *or* come from a configured list of loggers.
+    """
+
+    def __init__(self, acceptnames, acceptlevel):
+        """Initializes a Filter object"""
+        
+        self.acceptlevel = acceptlevel
+        self.acceptnames = [ x.lower() for x in acceptnames ]
+
+    def filter(self, record):
+        '''Determine if the log message should be printed'''
+
+        if record.levelno >= self.acceptlevel:
+            return True
+
+        if record.name.lower() in self.acceptnames:
+            return True
+
+        return False
+    
+def add_stdout_logging(quiet=False):
+    '''Add stdout logging handler to root logger'''
+
+    root_logger = logging.getLogger()
+    formatter = logging.Formatter('%(message)s') 
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    if quiet:
+        handler.setLevel(logging.WARN)
+    else:
+        handler.setLevel(logging.INFO)
+    root_logger.addHandler(handler)
+    return handler
+
+def get_seq_no(bucket):
+    '''Get current metadata sequence number'''   
+    from .backends.common import NoSuchObject
+    
+    seq_nos = list(bucket.list('s3ql_seq_no_')) 
+    if not seq_nos:
+        # Maybe list result is outdated
+        seq_nos = [ 's3ql_seq_no_1' ]
+    
+    if (seq_nos[0].endswith('.meta') 
+        or seq_nos[0].endswith('.dat')): 
+        raise QuietError('Old file system revision, please run `s3qladm upgrade` first.')
+         
+    seq_nos = [ int(x[len('s3ql_seq_no_'):]) for x in seq_nos ]
+    seq_no = max(seq_nos) 
+    
+    # Make sure that object really exists
+    while ('s3ql_seq_no_%d' % seq_no) not in bucket:
+        seq_no -= 1 
+        if seq_no == 0:
+            raise QuietError('No S3QL file system found in bucket.')
+    while ('s3ql_seq_no_%d' % seq_no) in bucket:
+        seq_no += 1 
+    seq_no -= 1
+    
+    # Delete old seq nos
+    for i in [ x for x in seq_nos if x < seq_no - 10 ]:
+        try:
+            del bucket['s3ql_seq_no_%d' % i]
+        except NoSuchObject:
+            pass # Key list may not be up to date
+        
+    return seq_no   
+
+def cycle_metadata(bucket):
+    from .backends.common import NoSuchObject
+    
+    log.info('Backing up old metadata...')
+    for i in reversed(range(10)):
+        try:
+            bucket.copy("s3ql_metadata_bak_%d" % i, "s3ql_metadata_bak_%d" % (i + 1))
+        except NoSuchObject:
+            pass
+                
+    bucket.copy("s3ql_metadata", "s3ql_metadata_bak_0")             
+
+def dump_metadata(ofh, conn):
+    
+    log.info('Dumping metadata...')
+    # First write everything into temporary file
+    tmp = tempfile.TemporaryFile()
+    pickler = pickle.Pickler(tmp, 2)
+    bufsize = 256
+    buf = range(bufsize)
+    tables_to_dump = [('objects', 'id'), ('blocks', 'id'),
+                      ('inode_blocks', 'inode, blockno'),
+                      ('inodes', 'id'), ('symlink_targets', 'inode'),
+                      ('names', 'id'), ('contents', 'parent_inode, name_id'),
+                      ('ext_attributes', 'inode, name')]
+
+    columns = dict()
+    for (table, _) in tables_to_dump:
+        columns[table] = list()
+        for row in conn.query('PRAGMA table_info(%s)' % table):
+            columns[table].append(row[1])
+
+    pickler.dump((tables_to_dump, columns))
+    
+    for (table, order) in tables_to_dump:
+        log.info('..%s..' % table)
+        pickler.clear_memo()
+        i = 0
+        for row in conn.query('SELECT %s FROM %s ORDER BY %s' 
+                              % (','.join(columns[table]), table, order)):
+            buf[i] = row
+            i += 1
+            if i == bufsize:
+                pickler.dump(buf)
+                pickler.clear_memo()
+                i = 0
+
+        if i != 0:
+            pickler.dump(buf[:i])
+        
+        pickler.dump(None)
+
+    # Then compress and send
+    log.info("Compressing and uploading metadata...")
+    compr = lzma.LZMACompressor(options={ 'level': 7 })
+    tmp.seek(0)
+    while True:
+        buf = tmp.read(BUFSIZE)
+        if not buf:
+            break
+        buf = compr.compress(buf)
+        if buf:
+            ofh.write(buf)
+    buf = compr.flush()
+    if buf:
+        ofh.write(buf)
+    del compr # Free memory ASAP, LZMA level 7 needs 186 MB
+    tmp.close()
+
+def restore_metadata(ifh, conn):
+
+    # Note: unpickling is terribly slow if fh is not a real file object, so
+    # uncompressing to a temporary file also gives a performance boost
+    log.info('Downloading and decompressing metadata...')
+    tmp = tempfile.TemporaryFile()
+    decompressor = lzma.LZMADecompressor()
+    while True:
+        buf = ifh.read(BUFSIZE)
+        if not buf:
+            break
+        buf = decompressor.decompress(buf)
+        if buf:
+            tmp.write(buf)
+    del decompressor
+    tmp.seek(0) 
+
+    log.info("Reading metadata...")
+    unpickler = pickle.Unpickler(tmp)
+    (to_dump, columns) = unpickler.load()
+    create_tables(conn)
+    for (table, _) in to_dump:
+        log.info('..%s..', table)
+        col_str = ', '.join(columns[table])
+        val_str = ', '.join('?' for _ in columns[table])
+        sql_str = 'INSERT INTO %s (%s) VALUES(%s)' % (table, col_str, val_str)
+        while True:
+            buf = unpickler.load()
+            if not buf:
+                break
+            for row in buf:
+                conn.execute(sql_str, row)
+
+    tmp.close()
+    conn.execute('ANALYZE')
+    
+class QuietError(Exception):
+    '''
+    QuietError is the base class for exceptions that should not result
+    in a stack trace being printed.
+    
+    It is typically used for exceptions that are the result of the user
+    supplying invalid input data. The exception argument should be a
+    string containing sufficient information about the problem.
+    '''
+    
+    def __init__(self, msg=''):
+        super(QuietError, self).__init__()
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+def setup_excepthook():
+    '''Modify sys.excepthook to log exceptions
+    
+    Also makes sure that exceptions derived from `QuietException`
+    do not result in stacktraces.
+    '''
+    
+    def excepthook(type_, val, tb):
+        root_logger = logging.getLogger()
+        if isinstance(val, QuietError):
+            root_logger.error(val.msg)
+        else:
+            root_logger.error('Uncaught top-level exception', 
+                              exc_info=(type_, val, tb))
+            
+    sys.excepthook = excepthook 
+    
+def inode_for_path(path, conn):
+    """Return inode of directory entry at `path`
+    
+     Raises `KeyError` if the path does not exist.
+    """
+    from .database import NoSuchRowError
+    
+    if not isinstance(path, bytes):
+        raise TypeError('path must be of type bytes')
+
+    # Remove leading and trailing /
+    path = path.lstrip(b"/").rstrip(b"/")
+
+    # Traverse
+    inode = ROOT_INODE
+    for el in path.split(b'/'):
+        try:
+            inode = conn.get_val("SELECT inode FROM contents_v WHERE name=? AND parent_inode=?", 
+                                 (el, inode))
+        except NoSuchRowError:
+            raise KeyError('Path %s does not exist' % path)
+
+    return inode
+
+def get_path(id_, conn, name=None):
+    """Return a full path for inode `id_`.
+    
+    If `name` is specified, it is appended at the very end of the
+    path (useful if looking up the path for file name with parent
+    inode).
+    """
+
+    if name is None:
+        path = list()
+    else:
+        if not isinstance(name, bytes):
+            raise TypeError('name must be of type bytes')
+        path = [ name ]
+
+    maxdepth = 255
+    while id_ != ROOT_INODE:
+        # This can be ambiguous if directories are hardlinked
+        (name2, id_) = conn.get_row("SELECT name, parent_inode FROM contents_v "
+                                    "WHERE inode=? LIMIT 1", (id_,))
+        path.append(name2)
+        maxdepth -= 1
+        if maxdepth == 0:
+            raise RuntimeError('Failed to resolve name "%s" at inode %d to path',
+                               name, id_)
+
+    path.append(b'')
+    path.reverse()
+
+    return b'/'.join(path)
+
+
+def _escape(s):
+    '''Escape '/', '=' and '\0' in s'''
+
+    s = s.replace('=', '=3D')
+    s = s.replace('/', '=2F')
+    s = s.replace('\0', '=00')
+
+    return s
+
+def get_bucket_cachedir(storage_url, cachedir):
+    if not os.path.exists(cachedir):
+        os.mkdir(cachedir, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+    return os.path.join(cachedir, _escape(storage_url))
+
+# Name and inode of the special s3ql control file
+CTRL_NAME = b'.__s3ql__ctrl__'
+CTRL_INODE = 2
+
+def sha256_fh(fh):
+    fh.seek(0)
+    
+    # Bogus error about hashlib not having a sha256 member
+    #pylint: disable=E1101
+    sha = hashlib.sha256()
+
+    while True:
+        buf = fh.read(BUFSIZE)
+        if not buf:
+            break
+        sha.update(buf)
+
+    return sha.digest()
+
+def init_tables(conn):
+    # Insert root directory
+    timestamp = time.time() - time.timezone
+    conn.execute("INSERT INTO inodes (id,mode,uid,gid,mtime,atime,ctime,refcount) "
+                 "VALUES (?,?,?,?,?,?,?,?)",
+                   (ROOT_INODE, stat.S_IFDIR | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+                   | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH,
+                    os.getuid(), os.getgid(), timestamp, timestamp, timestamp, 1))
+
+    # Insert control inode, the actual values don't matter that much 
+    conn.execute("INSERT INTO inodes (id,mode,uid,gid,mtime,atime,ctime,refcount) "
+                 "VALUES (?,?,?,?,?,?,?,?)",
+                 (CTRL_INODE, stat.S_IFIFO | stat.S_IRUSR | stat.S_IWUSR,
+                  0, 0, timestamp, timestamp, timestamp, 42))
+
+    # Insert lost+found directory
+    inode = conn.rowid("INSERT INTO inodes (mode,uid,gid,mtime,atime,ctime,refcount) "
+                       "VALUES (?,?,?,?,?,?,?)",
+                       (stat.S_IFDIR | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR,
+                        os.getuid(), os.getgid(), timestamp, timestamp, timestamp, 1))
+    name_id = conn.rowid('INSERT INTO names (name, refcount) VALUES(?,?)',
+                         (b'lost+found', 1))
+    conn.execute("INSERT INTO contents (name_id, inode, parent_inode) VALUES(?,?,?)",
+                 (name_id, inode, ROOT_INODE))
+
+def create_tables(conn): 
+    # Table of storage objects
+    # Refcount is included for performance reasons
+    conn.execute("""
+    CREATE TABLE objects (
+        id        INTEGER PRIMARY KEY AUTOINCREMENT,
+        refcount  INT NOT NULL, 
+        size      INT  
+    )""")
+
+    # Table of known data blocks
+    # Refcount is included for performance reasons
+    conn.execute("""
+    CREATE TABLE blocks (
+        id        INTEGER PRIMARY KEY,
+        hash      BLOB(16) UNIQUE,
+        refcount  INT NOT NULL,
+        size      INT NOT NULL,    
+        obj_id    INTEGER NOT NULL REFERENCES objects(id)
+    )""")
+                
+    # Table with filesystem metadata
+    # The number of links `refcount` to an inode can in theory
+    # be determined from the `contents` table. However, managing
+    # this separately should be significantly faster (the information
+    # is required for every getattr!)
+    conn.execute("""
+    CREATE TABLE inodes (
+        -- id has to specified *exactly* as follows to become
+        -- an alias for the rowid.
+        id        INTEGER PRIMARY KEY,
+        uid       INT NOT NULL,
+        gid       INT NOT NULL,
+        mode      INT NOT NULL,
+        mtime     REAL NOT NULL,
+        atime     REAL NOT NULL,
+        ctime     REAL NOT NULL,
+        refcount  INT NOT NULL,
+        size      INT NOT NULL DEFAULT 0,
+        rdev      INT NOT NULL DEFAULT 0,
+        locked    BOOLEAN NOT NULL DEFAULT 0
+    )""")
+
+    # Further Blocks used by inode (blockno >= 1)
+    conn.execute("""
+    CREATE TABLE inode_blocks (
+        inode     INTEGER NOT NULL REFERENCES inodes(id),
+        blockno   INT NOT NULL,
+        block_id    INTEGER NOT NULL REFERENCES blocks(id),
+        PRIMARY KEY (inode, blockno)
+    )""")
+    
+    # Symlinks
+    conn.execute("""
+    CREATE TABLE symlink_targets (
+        inode     INTEGER PRIMARY KEY REFERENCES inodes(id),
+        target    BLOB NOT NULL
+    )""")
+    
+    # Names of file system objects
+    conn.execute("""
+    CREATE TABLE names (
+        id     INTEGER PRIMARY KEY,
+        name   BLOB NOT NULL,
+        refcount  INT NOT NULL,
+        UNIQUE (name)
+    )""")
+
+    # Table of filesystem objects
+    # rowid is used by readdir() to restart at the correct position
+    conn.execute("""
+    CREATE TABLE contents (
+        rowid     INTEGER PRIMARY KEY AUTOINCREMENT,
+        name_id   INT NOT NULL REFERENCES names(id),
+        inode     INT NOT NULL REFERENCES inodes(id),
+        parent_inode INT NOT NULL REFERENCES inodes(id),
+        
+        UNIQUE (parent_inode, name_id)
+    )""")
+
+    # Extended attributes
+    conn.execute("""
+    CREATE TABLE ext_attributes (
+        inode     INTEGER NOT NULL REFERENCES inodes(id),
+        name      BLOB NOT NULL,
+        value     BLOB NOT NULL,
+ 
+        PRIMARY KEY (inode, name)               
+    )""")
+
+    # Shortcurts
+    conn.execute("""
+    CREATE VIEW contents_v AS
+    SELECT * FROM contents JOIN names ON names.id = name_id       
+    """)    
