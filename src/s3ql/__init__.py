56a0c3e19e2c3884f1a8ed236d111b40aed69777
Nikolaus Rath <Nikolaus@rath.org>
2012-01-16 17:35:06 -0500

    Started work on SWIFT backend.

---
+++ src/s3ql/__init__.py
@@ -0,0 +1,27 @@
+'''
+__init__.py - this file is part of S3QL (http://s3ql.googlecode.com)
+
+Copyright (C) 2008-2009 Nikolaus Rath <Nikolaus@rath.org>
+
+This program can be distributed under the terms of the GNU GPLv3.
+'''
+
+from __future__ import division, print_function
+
+__all__ = [ 'adm', 'backends', 'block_cache', 'cleanup_manager', 'common',
+            'cp', 'ctrl', 'daemonize', 'database', 'deltadump', '_deltadump',
+            'fs', 'fsck', 'inode_cache', 'lock', 'mkfs', 'mount', 'ordered_dict',
+            'parse_args', 'remove', 'statfs', 'umount', 'VERSION',
+            'CURRENT_FS_REV', 'REV_VER_MAP' ]
+
+# Also, remove obsolete rowid column from contents
+VERSION = '1.9-rc2'
+CURRENT_FS_REV = 15
+
+# Maps file system revisions to the last S3QL version that
+# supported this revision.
+REV_VER_MAP = { 15: '1.9',
+                14: '1.8.1',
+                13: '1.6',
+                12: '1.3',
+                11: '1.0.1' }
